6f90de1ab7a51c0e94faf0ee19a557b0493a932c
Gaurav Chaudhary <gauravmrjatt4@gmail.com>
2026-03-08 20:20:50 +0530

    updated the env parsing

--- server/services/firebase.service.js
+++ server/services/firebase.service.js
@@ -14,8 +14,10 @@ const initializeFirebase = () => {
 
   const serviceAccount = {
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+      .replace(/\\\\n/g, '\n')
+      .replace(/\\n/g, '\n'),
   };
   console.log(serviceAccount);
   admin.initializeApp({
